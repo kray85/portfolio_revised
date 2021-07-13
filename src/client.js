a5e5a7b9aa31bb0ba398f98e3ca9ffb37ff220be
Kendal <kray85@gmail.com>
2021-07-13 10:29:07 -0600

    added sanity api version

--- src/client.js
+++ src/client.js
@@ -2,6 +2,9 @@ import sanityClient from '@sanity/client'
 
 export default sanityClient({
     projectId: 'z5umnzzh',
-    dataset: 'production'
+    dataset: 'production',
+    apiVersion: '2021-07-13',
+    token: '',
+    useCdn: true,
 })
 
